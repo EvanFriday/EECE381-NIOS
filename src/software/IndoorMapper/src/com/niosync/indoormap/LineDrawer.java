165d44dfc813f5ac9c9720a8267b3f1d6b3cbbc5
EvanFriday <Evan_Friday@hotmail.com>
2014-03-11 19:56:03 -0700

    testing new function add/ ignores are working properly

---
+++ src/software/IndoorMapper/src/com/niosync/indoormap/LineDrawer.java
@@ -0,0 +1,37 @@
+package com.niosync.indoormap;
+
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
+
+public class LineDrawer extends Drawable {
+
+	public LineDrawer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void draw(Canvas arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getOpacity() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setAlpha(int arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setColorFilter(ColorFilter arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
