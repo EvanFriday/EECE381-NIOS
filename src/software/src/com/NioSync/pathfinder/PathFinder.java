639a6d364b556dbc801463285c4503444635a310
Evan Friday <evan_friday@hotmail.com>
2014-03-17 00:10:35 -0700

    added temporary view/button

--- src/software/src/com/NioSync/pathfinder/PathFinder.java
+++ src/software/src/com/NioSync/pathfinder/PathFinder.java
@@ -1,20 +1,46 @@
 package com.NioSync.pathfinder;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class PathFinder extends Activity {
 
-	EditText start_loc,dest_loc;
-	
+	private EditText start_loc,dest_loc;
+	private Button view_map;
+	private ImageView map;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_path_finder);
+        
+		
+        this.view_map = (Button) this.findViewById(R.id.view_map);
+        this.view_map.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				viewMap();
+			}
+		});
+  
     }
-
+    
+    
+    public void viewMap(){
+    	this.map = (ImageView) this.findViewById(R.id.map_container);
+    	if (map.getVisibility()==View.VISIBLE)
+    		map.setVisibility(View.INVISIBLE);
+    	else
+    		map.setVisibility(View.VISIBLE);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
