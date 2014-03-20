f19c0ac9297221d81ceabb001a4998187c3cd327
EvanFriday <Evan_Friday@hotmail.com>
2014-03-20 03:47:59 -0700

    fixed the pathfinding view to scale to screen size, removed title bar because I felt like it, TODO for line drawing, other minor edits

--- src/software/src/com/NioSync/pathfinder/PathFinder.java
+++ src/software/src/com/NioSync/pathfinder/PathFinder.java
@@ -4,6 +4,7 @@ package com.NioSync.pathfinder;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -13,29 +14,13 @@ import android.widget.ImageView;
 public class PathFinder extends Activity {
 
 	private EditText start_loc,dest_loc;
-	private Button view_map;
 	private ImageView map;
     @Override	
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_path_finder);        
-	
-        this.view_map = (Button) this.findViewById(R.id.view_map);
-        this.view_map.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				viewMap();
-			}
-		}); 
-    }
+        setContentView(R.layout.activity_path_finder);
+       
     
-    
-    public void viewMap(){
-    	this.map = (ImageView) this.findViewById(R.id.map_container);
-    	if (map.getVisibility()==View.VISIBLE)
-    		map.setVisibility(View.INVISIBLE);
-    	else
-    		map.setVisibility(View.VISIBLE);
     }
 
     @Override
