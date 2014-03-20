e52a56cbe6a80adf6797bc76fd58ee91d5383641
EvanFriday <Evan_Friday@hotmail.com>
2014-03-20 05:02:41 -0700

    created dropdown for searchbar

--- src/software/src/com/NioSync/pathfinder/PathFinder.java
+++ src/software/src/com/NioSync/pathfinder/PathFinder.java
@@ -7,19 +7,35 @@ import android.os.Bundle;
 import android.util.TypedValue;
 import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class PathFinder extends Activity {
 
 	private EditText start_loc,dest_loc;
 	private ImageView map;
+	private ImageButton pulldown;
+	private LinearLayout pulldown_container;
+	private RelativeLayout pathfinder_rel;
     @Override	
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_path_finder);
+       pulldown = (ImageButton) findViewById(R.id.pulldown);
+ 
        
+       pulldown.setOnClickListener(new View.OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			showHideTools();
+			
+			
+		}
+	});
     
     }
 
@@ -28,6 +44,32 @@ public class PathFinder extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.path_finder, menu);
         return true;
+    }
+    
+    public void showHideTools(){
+        this.pulldown_container = (LinearLayout) findViewById(R.id.linearLayout_pulldowncontainer);
+    	this.pathfinder_rel = (RelativeLayout) findViewById(R.id.relativelayout_pathfinder);
+        if(this.pulldown_container.getVisibility()==View.VISIBLE){
+    		
+        	this.pulldown_container.setVisibility(View.GONE);
+    		for ( int i = 0; i < pulldown_container.getChildCount();  i++ ){
+    		    View view = pulldown_container.getChildAt(i);
+    		    view.setVisibility(View.GONE); // Or whatever you want to do with the view.
+    		}
+    		pathfinder_rel.invalidate();
+    		this.pulldown.setImageResource(R.drawable.pulldown_bar);
+    	}
+    	else{
+    		
+    		this.pulldown_container.setVisibility(View.VISIBLE);
+    		for ( int i = 0; i < pulldown_container.getChildCount();  i++ ){
+    		    View view = pulldown_container.getChildAt(i);
+    		    view.setVisibility(View.VISIBLE); // Or whatever you want to do with the view.
+    		}
+    		pathfinder_rel.invalidate();
+    		this.pulldown.setImageResource(R.drawable.pulldown_bar_extended);
+    		
+    	}
     }
     
 }
