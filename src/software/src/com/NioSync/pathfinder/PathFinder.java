6fbce72644b162dfcad35cce58458f3e67cfd206
EvanFriday <Evan_Friday@hotmail.com>
2014-03-20 16:15:51 -0700

    created spinner init function

--- src/software/src/com/NioSync/pathfinder/PathFinder.java
+++ src/software/src/com/NioSync/pathfinder/PathFinder.java
@@ -4,14 +4,17 @@ package com.NioSync.pathfinder;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.Spinner;
 
 public class PathFinder extends Activity {
 
@@ -20,12 +23,19 @@ public class PathFinder extends Activity {
 	private ImageButton pulldown;
 	private LinearLayout pulldown_container;
 	private RelativeLayout pathfinder_rel;
+	private Map map_object;
+	private Spinner start_loc_spin,dest_loc_spin;
     @Override	
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    	super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_path_finder);
+        map_object = new Map();
        pulldown = (ImageButton) findViewById(R.id.pulldown);
- 
+       start_loc_spin = (Spinner) findViewById(R.id.startLoc);
+       
+       
+       
+       
        
        pulldown.setOnClickListener(new View.OnClickListener() {
 		
@@ -70,6 +80,17 @@ public class PathFinder extends Activity {
     		this.pulldown.setImageResource(R.drawable.pulldown_bar_extended);
     		
     	}
+    }
+    public void spindownPopulate(Spinner spin){
+    	int count = map_object.getNodeNames().size();
+        String[] node_names = new String[count];
+        map_object.getNodeNames().copyInto(node_names); 
+        for(int i=0;i<count;i++){
+        Log.e("ARRAY CONTAINS",node_names[i]);
+        }
+        
+        ArrayAdapter<String> spinner_adapter_start = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, node_names);
+        start_loc_spin.setAdapter(spinner_adapter_start);
     }
     
 }
