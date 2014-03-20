e92c77cceace8fa1ecb3ac0a863b7b04ff309803
g16-sw <mcw918@hotmail.com>
2014-03-20 01:31:27 -0700

    clean up the code

--- src/system/Software/Pathfinder/Pathfinder.c
+++ src/system/Software/Pathfinder/Pathfinder.c
@@ -24,32 +24,18 @@ int main() {
 	}
 
 	printf("Sending the message to the Middleman\n");
-	// Start with the number of bytes in our message
 	readsendfile("nodes.xml", uart);
-	/*alt_up_rs232_write_data(uart, (unsigned char) strlen(message));
-	 // Now send the actual message to the Middleman
-	 for (i = 0; i < strlen(message); i++) {
-	 alt_up_rs232_write_data(uart, message[i]);
-	 }*/
 	// Now receive the message from the Middleman
 	printf("Waiting for data to come back from the Middleman\n");
 	while (alt_up_rs232_get_used_space_in_read_FIFO(uart) == 0)
 		;
-	// First byte is the number of characters in our message
-	/*alt_up_rs232_read_data(uart, &data, &parity);
-	 int num_to_receive = (int) data;*/
-	//printf("About to receive %d characters:\n", num_to_receive);
+
 	printf("About to receive characters:\n");
 	while (alt_up_rs232_get_used_space_in_read_FIFO(uart)) {
 		alt_up_rs232_read_data(uart, &data, &parity);
 		printf("%c", data);
 	}
-	/*for (i = 0; i < num_to_receive; i++) {
-	 while (alt_up_rs232_get_used_space_in_read_FIFO(uart) == 0)
-	 ;
-	 alt_up_rs232_read_data(uart, &data, &parity);
-	 printf("%c", data);
-	 }*/
+
 	printf("\n");
 	printf("Message Echo Complete\n");
 	return 0;
