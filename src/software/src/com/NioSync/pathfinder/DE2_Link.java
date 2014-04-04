29d57cc7091657ecf1085a2ae34d00881aef2884
SMWTLM <mcw918@hotmail.com>
2014-04-03 20:57:11 -0700

    further working being done on communication

--- src/software/src/com/NioSync/pathfinder/DE2_Link.java
+++ src/software/src/com/NioSync/pathfinder/DE2_Link.java
@@ -13,9 +13,11 @@ import java.net.UnknownHostException;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import com.example.ece381.MainActivity;
 import com.example.ece381.MyApplication;
 import com.example.ece381.R;
 import com.example.ece381.MainActivity.SocketConnect;
+import com.example.ece381.MainActivity.TCPReadTimerTask;
 
 import android.app.Activity;
 import android.content.Context;
@@ -29,6 +31,30 @@ import android.widget.TextView;
 
 public class DE2_Link extends Activity {
 
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		// This call will result in better error messages if you
+		// try to do things in the wrong thread.
+
+		StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+				.detectDiskReads().detectDiskWrites().detectNetwork()
+				.penaltyLog().build());
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.get_map);
+
+		EditText et = (EditText) findViewById(R.id.Transfer_Progress);
+		et.setKeyListener(null);
+
+		// Set up a timer task. We will use the timer to check the
+		// input queue every 500 ms
+
+/*		TCPReadTimerTask tcp_task = new TCPReadTimerTask();
+		Timer tcp_timer = new Timer();
+		tcp_timer.schedule(tcp_task, 3000, 500);*/
+	}
+	
 	public void openSocket(View view) {
 		DE2_Class app = (DE2_Class) getApplication();
 		TextView msgbox = (TextView) findViewById(R.id.Transfer_Progress);
@@ -47,8 +73,62 @@ public class DE2_Link extends Activity {
 		new SocketConnect().execute((Void) null);
 	}
 
-	public void OnReceive(View view) {
+	public class SocketConnect extends AsyncTask<Void, Void, Socket> {
 
+		// The main parcel of work for this thread. Opens a socket
+		// to connect to the specified IP.
+
+		protected Socket doInBackground(Void... voids) {
+			Socket s = null;
+			String ip = getConnectToIP();
+			Integer port = getConnectToPort();
+
+			try {
+				s = new Socket(ip, port);
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return s;
+		}
+
+		// After executing the doInBackground method, this is
+		// automatically called, in the UI (main) thread to store
+		// the socket in this app's persistent storage
+
+		protected void onPostExecute(Socket s) {
+			DE2_Class myApp = (DE2_Class) DE2_Link.this.getApplication();
+			myApp.sock = s;
+		}
+	}
+
+	// Called when the user closes a socket
+
+	public void closeSocket(View view) {
+		DE2_Class app = (DE2_Class) getApplication();
+		Socket s = app.sock;
+		try {
+			s.getOutputStream().close();
+			s.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// Construct an IP address from the four boxes
+
+	public String getConnectToIP() {
+		String addr = "192.168.1.1";
+		return addr;
+	}
+
+	// Gets the Port from the appropriate field.
+
+	public Integer getConnectToPort() {
+		Integer port;
+		port = 50002;
+		return port;
 	}
 
 	void getMapNodes() {
