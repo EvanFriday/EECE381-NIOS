54617712bd0069d6b9f3773bab513b1def166cca
SteveQiu <qiuyuliang20042005@hotmail.com>
2014-04-07 00:03:14 -0700

    Split action bar and add search and option

--- src/software/src/com/NioSync/pathfinder/PathFinder.java
+++ src/software/src/com/NioSync/pathfinder/PathFinder.java
@@ -13,6 +13,8 @@ import android.graphics.Rect;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.AdapterView;
@@ -139,10 +141,31 @@ public class PathFinder extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.path_finder, menu);
-		return true;
+	    // Inflate the menu items for use in the action bar
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.path_finder, menu);
+	    return super.onCreateOptionsMenu(menu);
 	}
+	
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle presses on the action bar items
+	    switch (item.getItemId()) {
+	        case R.id.action_search:
+	            
+	            return true;
+	        case R.id.action_settings:
+	            
+	            return true;
+	        case R.id.action_location:
+	        	
+	        	return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
+	
 
 	public void showHideTools() {
 		this.pulldown_container = (LinearLayout) findViewById(R.id.linearLayout_pulldowncontainer);
