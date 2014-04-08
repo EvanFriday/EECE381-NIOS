d02d761b6d13b6575eb575508b5f8cd91ecdd461
SteveQiu <qiuyuliang20042005@hotmail.com>
2014-04-08 04:49:35 -0700

    Modify GUI to fix the extra GO to map listview

--- src/software/src/com/NioSync/pathfinder/MainMenu.java
+++ src/software/src/com/NioSync/pathfinder/MainMenu.java
@@ -49,7 +49,7 @@ public class MainMenu extends Activity {
         
         ListView menuList = (ListView) findViewById(R.id.listview_menu);
         String [] menu_items = {
-        		getResources().getString(R.string.to_map),
+        		//getResources().getString(R.string.to_map),
         		getResources().getString(R.string.download_map),
         		getResources().getString(R.string.to_map_select),
         		getResources().getString(R.string.to_qrscan),
